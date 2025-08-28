e00326cfe0883bb5d756d92d607b2f67f70c4c77
Bladimir <bladimir.escalera@gmail.com>
2025-08-28 18:14:52 -0400

    habilitando swager

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+//swager
+
+const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory);
+  
+
+//swager
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
